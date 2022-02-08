9d1a47dcd79191da752766268456b1582f319cc8
Steve Canneaux <seamlessloops@gmail.com>
2022-02-08 20:16:42 +0000

    Add files via upload

--- Examples of logical operators.py
+++ Examples of logical operators.py
@@ -1,1 +1,23 @@
+A = 10
+B = 50
+if ((A < 10) && (B <= 100)) :
+ print("This message")
+print("Program 1 ends here")
 
+A = 4
+B = 40
+if ((A < 10) || (B <= 100)) :
+ print("This message")
+print("Program 2 ends here")
+
+A = 2
+B = 100
+if ((A < 10) && (B <= 100)) :
+ print("This message")
+print("Program 3 ends here")
+
+A = 10
+B = 400
+if ((A < 10) || (B <= 100)) :
+ print("This message")
+print("Program 4 ends here")
